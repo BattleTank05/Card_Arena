762b78af5937fbc01a60bd1907d98a048437bccb
Josh Gore <joshjamesgore@gmail.com>
2020-09-25 20:57:05 -0700

    new changes

---
+++ Card Arena/Card Arena/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Card_Arena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
